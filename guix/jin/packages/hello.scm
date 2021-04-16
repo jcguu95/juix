37cbbcf5d727e79042bc6687065e7a234c64f491
Jin <jcguu95@gmail.com>
2021-04-16 10:42:57 -0500

    first package for testing!

---
+++ guix/jin/packages/hello.scm
@@ -0,0 +1,21 @@
+;; Taken from https://guix.gnu.org/cookbook/en/html_node/A-_0060_0060Hello-World_0027_0027-package.html
+;;
+(define-public hello
+  (package
+   (name "hello")
+   (version "2.10")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "mirror://gnu/hello/hello-" version
+                                ".tar.gz"))
+            (sha256
+             (base32
+              "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i"))))
+   (build-system gnu-build-system)
+   (synopsis "Hello, GNU world: An example GNU package")
+   (description
+    "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+serves as an example of standard GNU coding practices.  As such, it supports
+command-line arguments, multiple languages, and so on.")
+   (home-page "https://www.gnu.org/software/hello/")
+   (license gpl3+)))
